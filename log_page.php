85dd52dd5e0ec098c7e6f53554ee853a1f677d03
DmytroRaiko <55841333+DmytroRaiko@users.noreply.github.com>
2021-07-25 12:35:20 +0300

    Dmitriy h (#7)
    * Sign Form Start
    
    * Sign Form (design without resizes)
    
    Co-authored-by: Dmytro-Hlukhovtsov <67894034+Dmytro-Hlukhovtsov@users.noreply.github.com>

---
+++ log_page.php
@@ -0,0 +1,17 @@
+<?php include_once("function.php") ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<?php
+    head();
+?>
+<body>
+    
+    <div id="log-page-main-block">
+        <?php
+            sign_form();
+        ?>
+    </div>
+</body>
+
+</html>
