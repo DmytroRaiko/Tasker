31e6f2baf584a3598140673b3c47ae52913ccbb6
DmytroRaiko <55841333+DmytroRaiko@users.noreply.github.com>
2021-07-29 15:36:01 +0300

    modal-create finish

--- templates/head.php
+++ templates/head.php
@@ -4,7 +4,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="https://unpkg.com/flatpickr/dist/flatpickr.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="templates/select2/dist/js/select2.min.js"></script>
+    <script src="templates/select2/dist/js/i18n/ru.js"></script>
+    <script src="https://unpkg.com/flatpickr"></script>
+
 
     <title>tasker</title>
 </head>
